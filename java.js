516f00d6a69f7db1a5a662ed4f16576141055e39
amr-Bekhit <gamalhassan1996525@gmail.com>
2022-07-06 06:39:09 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,457 @@
+
+            // as 1
+
+// var firestNum = window.prompt("enter number");
+// window.alert(Number(firestNum));
+
+
+            // as 2
+
+// var num1 = window.prompt("enter your num1");
+// var num2 = window.prompt("enter your num2");
+// if(num1<num2)
+// {
+//     window.alert(num2);
+// }
+// else if(num2<num1)
+// {
+//     window.alert(num1);
+// }
+
+
+            // as 3
+
+
+// var num = window.prompt("num");
+// if(num %3==0 && num %4==0)
+// {
+//     alert("yes");
+// }
+// else
+// {
+//     alert("no");
+// }
+
+
+        // as 4
+
+// var num = window.prompt('enter your number');
+// if( num > 0 )
+// {
+//     document.getElementById("demo").innerHTML="positive";
+// }
+// else
+// {
+//     document.getElementById('demo').innerHTML="negative";
+// }
+
+
+
+
+        // as 5
+
+
+        // var num1 = Number (window.prompt('Enter First Number '));
+        // var num2 = Number (window.prompt('Enter Second Number '));
+        // var num3 = Number (window.prompt('Enter Third Number '));
+        
+        // if(num1 > num2 && num2 > num3)
+        // {
+        //     window.alert('Max Element = ' + num1);
+        //     window.alert('Min Element = ' + num3);
+        // }
+        // else if(num1 > num2 && num3> num2)
+        // {
+        //     window.alert('Max Element = ' + num1);
+        //     window.alert('Min Element = ' + num2);
+        // }
+        
+        // else if (num2 > num3 && num3 > num1)
+        // {
+        //     window.alert('Max Element = ' + num2);
+        //     window.alert('Min Element = ' + num1);
+        // }
+        // else if (num2 > num1 && num1 > num3)
+        // {
+        //     window.alert('Max Element = ' + num2);
+        //     window.alert('Min Element = ' + num3);
+        // }
+        
+        // else if ( num3 > num1 && num1 > num2)
+        // {
+        //     window.alert('Max Element = ' + num3);
+        //     window.alert('Min Element = ' + num2);
+        // }
+        // else if ( num3 > num2 && num2> num1)
+        // {
+        //     window.alert('Max Element = ' + num3);
+        //     window.alert('Min Element = ' + num1);
+        // }
+
+
+
+
+
+
+
+// var num1 = Number (window.prompt('Please Enter Your First Integer Number :'));
+// var num2 = Number (window.prompt('Please Enter Your Second Integer Number :'));
+// var num3 = Number (window.prompt('Please Enter Your  Third Integer Number :'));
+// if(num1 > num2)
+// {
+//     if(num2 > num3)
+//     {
+//         window.alert('Max Element = ' + num1);
+//         window.alert('Min Element = ' + num3);
+//     }
+//     else
+//     {
+//         window.alert('Max Element = ' + num1);
+//         window.alert('Min Element = ' + num2);
+//     }
+// }
+// else if (num2 > num3 )
+// {
+//     if(num3 > num1)
+//     {
+//         window.alert('Max Element = ' + num2);
+//         window.alert('Min Element = ' + num1);
+//     }
+//     else
+//     {
+//         window.alert('Max Element = ' + num2);
+//         window.alert('Min Element = ' + num3);
+//     }
+// }
+// else if(num3 > num1)
+// {
+//     if(num1 > num2)
+//     {
+//         window.alert('Max Element = ' + num3);
+//         window.alert('Min Element = ' + num2);
+//     }
+//     else
+//     {
+//         window.alert('Max Element = ' + num3);
+//         window.alert('Min Element = ' + num1);
+//     }
+// }
+
+
+
+
+
+                // as 7
+
+
+
+
+// var num = window.prompt("enter number")
+// if(num %2==0)
+// {
+//     window.alert('Output: even')
+// }
+// else
+// {
+//     window.alert('Output: odd')
+// }
+
+
+
+                // as 8
+
+
+
+// var character = window.prompt("enter litter")
+// if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u')
+// {
+//     window.alert('Output: vowel ');
+// }
+// else
+// {
+//     window.alert('Output: Consonant ');
+// }
+
+
+
+            // as 9
+
+
+// var y = window.prompt('enter number');
+// for(x=1; x < y ;x+=1)
+// {
+//     window.alert(x)
+// }
+
+
+            // 10
+
+
+// var num = Number(window.prompt('enter number'))
+// for ( var i = 1 ; i <= 12 ; i ++)
+// { var result = num * i ;
+//  window.alert(result);
+// }
+
+
+            // 11
+// var num = window.prompt("enter number");
+// for (i=1 ; i<num ; i++)
+// if(i%2==0)
+// {
+//         window.alert(i)
+// }
+
+
+                // 12
+
+
+// var x = window.prompt('enter num1');
+// var y = window.prompt('enter num2');
+// var resalt = Math.pow(x, y);
+
+// window.alert(resalt);
+
+
+
+                // 13
+
+
+// var sup1 = Number( window.prompt('Enter physics mark'));
+// var sup2 = Number( window.prompt('Enter chemistry mark'));
+// var sup3 = Number( window.prompt('Enter math mark'));
+// var sup4 = Number( window.prompt('Enter english mark'));
+// var sup5 = Number( window.prompt('Enter history mark'));
+
+// var total = ( sup1 + sup2 + sup3 + sup4 + sup5);
+// window.alert('total marks = ' + total);
+// var average = (total / 5.0 );
+// console.log('Average = ' + average)
+// var percentage =((total / 500.0 ) *100)
+// window.alert('percentage = ' + percentage + '%' )
+
+
+                // 14
+
+
+// var x = window.prompt('- Month Number:')
+// if(x==1 || x==3 || x==5 || x==7 || x==8 || x==10 || x==12)
+// {
+// window.alert(31)
+// }
+// else if(x==2)
+// {
+// window.alert(28)
+// }
+// else if(x==4 || x==6 || x==9 || x==11)
+// {
+// window.alert(30)
+// }
+
+                // 15
+
+
+
+// var sup1 = Number( window.prompt('Enter physics mark'));
+// var sup2 = Number( window.prompt('Enter chemistry mark'));
+// var sup3 = Number( window.prompt('Enter math mark'));
+// var sup4 = Number( window.prompt('Enter english mark'));
+// var sup5 = Number( window.prompt('Enter history mark'));
+
+// var total = ( sup1 + sup2 + sup3 + sup4 + sup5);
+// var percentage = total *100 / 100 + "%";
+
+// if( percentage >= '90%')
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad A");
+// }
+// else if( percentage >= '80%')
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad B");
+// }
+// else if( percentage >= '70%')
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad C");
+// }
+// else if( percentage >= '60%')
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad D");
+// }
+// else if( percentage >= '40%')
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad E");
+// }
+// else
+// {
+//     window.alert("Percentage = "+ percentage +' : ' + "Grad F");
+
+// }
+
+
+
+
+
+                // 16
+
+
+// var x =Number( window.prompt('- Month Number:'))
+// switch(x)
+// {
+//         case 1 :
+// window.alert(31);
+//          break;
+//         case 2 :
+// window.alert(28);
+//          break;
+//         case 3 :
+// window.alert(31);
+//          break;
+//         case 4 :
+// window.alert(30);
+//          break;
+//         case 5 :
+// window.alert(31);
+//          break;
+//         case 6 :
+// window.alert(30);
+//          break;
+//         case 7 :
+// window.alert(31);
+//          break;
+//         case 8 :
+// window.alert(31);
+//          break;
+//         case 9 :
+// window.alert(30);
+//          break;
+//         case 10 :
+// window.alert(31);
+//          break;
+//         case 11 :
+// window.alert(30);
+//          break;
+//         case 12 :
+// window.alert(31);
+//          break;
+        
+
+// }
+
+
+                // 17
+
+
+
+// var x = window.prompt('- enter litter:')
+// switch(x)
+// {
+//         case "a" :
+//         window.alert(' vowel');
+//         break;
+//         case "e" :
+//         window.alert(' vowel');
+//         break;
+//         case "i" :
+//         window.alert(' vowel');
+//         break;
+//         case "o" :
+//         window.alert(' vowel');
+//         break;
+//         case "u" :
+//         window.alert(' vowel');
+//         break;
+//         default:
+//         window.alert(' consonant ');
+// }
+
+
+
+                // 18
+
+
+// var x =Number( window.prompt('- Month Number:'))
+// var y =Number( window.prompt('- Month Number:'))
+// switch (x>y)
+// {
+//         case  true:
+//         window.alert(x);
+//         break;
+//         case false:
+//         window.alert(y)
+// }
+
+
+                // 19
+
+
+// var x =Number( window.prompt('- enter Number:'))
+// switch(x ==0 )
+// {
+//         case true:
+//         window.alert(' zerooooo');
+//         break;
+//         case false:
+//         switch(x %2)
+// {
+//         case 1 :
+//         window.alert('odd');
+//         break;
+//         case 0 :
+//         window.alert('even');
+// }
+// }
+
+
+
+
+                // 20
+
+
+
+
+// var x =Number( window.prompt('- enter Number:'))
+// switch(x==0)
+// {
+//         case true:
+//         window.alert('zeroooo');
+//         break
+//         case false:
+//         switch(x>0)
+// {
+//         case true:
+//         window.alert('positive');
+//         break;
+//         case false:
+//         window.alert('negative');
+// }
+// }
+
+
+
+
+                // 21
+
+
+
+var x =Number( window.prompt('- enter Number:'));
+var y = window.prompt('- enter Number:');
+var z =Number( window.prompt('- enter Number:'));
+var resalt ;
+switch(y)
+{
+        case '+':
+        resalt = x + z ;
+        window.alert(resalt);
+        break
+        case '-':
+        resalt = x - z ;
+        window.alert(resalt);
+        break
+        case '*':
+        resalt = x * z ;
+        window.alert(resalt);
+        break;
+        case '/':
+        resalt = x / z ;
+        window.alert(resalt)
+}
